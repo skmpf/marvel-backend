99c20cd4a792bad2b4b61db21311040b2d4450a3
Sebastien Kempf <s.kempf@live.com>
2020-03-31 11:54:49 +0200

    updated readme

---
+++ README.md
@@ -0,0 +1,95 @@
+# Marvel API
+
+## Overview
+
+A simple API to access Marvel characters and comics from Marvel's very own (and sadly very slow) API
+
+<p align="center">
+	<img
+			width="600"
+			alt="demo"
+			src="https://github.com/sebkpf/marvel-react/blob/master/documentation/demo.png">
+</p>
+
+<p align="center">
+  Demo:<a href="https://wiki-marvel.netlify.com" target="_blank"> https://wiki-marvel.netlify.com</a>
+</p>
+
+### Server
+
+#### Dependencies
+
+- Node.js
+- axios
+- express
+- express-formidable
+- cors
+- mongoose
+- crypto-js
+- js-md5
+- uid2
+- dotenv
+
+#### Architecture
+
+- marvel route:
+
+      	- get characters: axios request to Marvel API
+      	- get character by id: axios request to Marvel API
+      	- get comics: axios request to Marvel API
+      	- get search for characters or comics: axios request to Marvel API
+      	- get user favorites (handled by cookies): axios request to Marvel API
+
+  Marvel API: https://developer.marvel.com/
+
+- user route:
+
+      	- sign up: creates new user account in MongoDB database and handles security with token, hash and salt
+      	- log in: handled by token
+
+- middleware: isAuthenticated checks for user token when client makes a http request
+
+### Running the project
+
+Clone this repository:
+
+```
+git clone https://github.com/sebkpf/marvel-backend.git
+cd marvel-backend
+```
+
+Install packages:
+
+```
+npm install
+```
+
+When installation is complete, run with:
+
+```bash
+npx nodemon index.js
+```
+
+## Marvel Client
+
+<a href="https://github.com/sebkpf/marvel-react">https://github.com/sebkpf/marvel-react</a>
+
+- React
+- HTTP request with axios package (get and post)
+- Hooks (useState, useEffect)
+- React Router Dom
+- Cookies to handle user favorites
+
+## Deployment
+
+- Client deployed with Netlify
+- Server deployed with Heroku
+- MongoDB database hosted on mLab
+
+## Project status
+
+Project is finished
+
+## Contact
+
+<a href="https://www.linkedin.com/in/sebastienkempf/" target="_blank">My LinkedIn profile</a>
--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.use(formidable({ multiples: true }));
 const mongoose = require("mongoose");
 const cors = require("cors");
 app.use(cors());
-const axios = require("axios");
-const md5 = require("js-md5");
-const uid2 = require("uid2");
 
 // set mongodb connection
 mongoose.connect(process.env.MONGODB_URI, {
@@ -18,121 +15,9 @@ mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true
 });
 
-// Marvel API requirements
-const privateKey = process.env.MARVEL_SKEY;
-const publicKey = process.env.MARVEL_PKEY;
-
 // import of User routes
 const UserRoutes = require("./routes/user");
 app.use(UserRoutes);
-
-// GET all characters
-app.get("/characters", async (req, res) => {
-  try {
-    let timeStamp = uid2(8);
-    let hash = md5(timeStamp + privateKey + publicKey);
-    const page = req.query.page;
-    const limit = 100;
-    const offset = limit * (page - 1);
-    const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&orderBy=name&limit=${limit}&offset=${offset}`
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-// GET one character by ID
-app.get("/characters/:id", async (req, res) => {
-  try {
-    let timeStamp = uid2(8);
-    let hash = md5(timeStamp + privateKey + publicKey);
-    const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/characters/${req.params.id}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-// GET all comics
-app.get("/comics", async (req, res) => {
-  try {
-    let timeStamp = uid2(8);
-    let hash = md5(timeStamp + privateKey + publicKey);
-    const page = req.query.page;
-    const limit = 100;
-    const offset = limit * (page - 1);
-    const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&orderBy=title&limit=${limit}&offset=${offset}`
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-// SEARCH for characters or comics
-app.get("/search/:category/:search", async (req, res) => {
-  try {
-    let timeStamp = uid2(8);
-    let hash = md5(timeStamp + privateKey + publicKey);
-    const category = req.params.category;
-    const search = req.params.search;
-    const page = req.query.page;
-    const limit = 100;
-    const offset = limit * (page - 1);
-
-    let response;
-    if (category === "characters") {
-      response = await axios.get(
-        `http://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&orderBy=name&limit=${limit}&offset=${offset}&nameStartsWith=${search}`
-      );
-    } else {
-      response = await axios.get(
-        `http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&orderBy=title&limit=${limit}&offset=${offset}&titleStartsWith=${search}`
-      );
-    }
-    const results = response.data.data;
-    res.json(results);
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-// GET favorites
-app.post("/favorites", async (req, res) => {
-  let timeStamp = uid2(8);
-  let hash = md5(timeStamp + privateKey + publicKey);
-
-  const fav = req.fields.fav;
-  let favTab = [[], []];
-
-  try {
-    for (let i = 0; i < fav.length; i++) {
-      if (i === 0) {
-        for (let j = 0; j < fav[i].length; j++) {
-          const response = await axios.get(
-            `http://gateway.marvel.com/v1/public/characters/${fav[i][j]}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
-          );
-          favTab[0].push(response.data);
-        }
-      } else {
-        for (let j = 0; j < fav[i].length; j++) {
-          const response = await axios.get(
-            `http://gateway.marvel.com/v1/public/comics/${fav[i][j]}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
-          );
-          favTab[1].push(response.data);
-        }
-      }
-    }
-    res.json(favTab);
-  } catch (error) {
-    res.json(error.message);
-  }
-});
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello Marvel" });
